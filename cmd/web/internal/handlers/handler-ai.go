4c35457c0de1e1f537b85578803e5a458d8093e3
davidhalasz <davidhalasz22@gmail.com>
2024-02-02 19:53:19 +0100

    added animated canvas

--- cmd/web/internal/handlers/handler-ai.go
+++ cmd/web/internal/handlers/handler-ai.go
@@ -1,13 +1,98 @@
 package handlers
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/davidhalasz/gomath/cmd/web/internal/render"
 )
 
 func AiPage(w http.ResponseWriter, r *http.Request) {
-	if err := render.Template(w, r, "mi.page.gohtml", nil); err != nil {
+	if err := render.Template(w, r, "ai-basics.page.gohtml", nil); err != nil {
 		app.ErrorLog.Println(err)
 	}
+}
+
+type Node struct {
+	Val   int
+	Left  *Node
+	Right *Node
+	Next  *Node
+}
+
+type NodeWithLevel struct {
+	node  *Node
+	level int
+}
+
+func BFS(root *Node) *Node {
+	if root == nil {
+		return root
+	}
+
+	queue := []NodeWithLevel{
+		{
+			node:  root,
+			level: 0,
+		},
+	}
+
+	visited := []*Node{}
+
+	for len(queue) > 0 {
+		vertex := queue[0]
+		node, level := vertex.node, vertex.level
+		visited = append(visited, node)
+		queue = queue[1:]
+
+		if node.Left != nil {
+			leftNode := NodeWithLevel{
+				node:  node.Left,
+				level: level + 1,
+			}
+			queue = append(queue, leftNode)
+
+			rightNode := NodeWithLevel{
+				node:  node.Right,
+				level: level + 1,
+			}
+			queue = append(queue, rightNode)
+		}
+
+		fmt.Printf("Level: %d", queue[len(queue)-1].level)
+		fmt.Printf("Visited: [ ")
+		for _, node := range visited {
+			fmt.Printf("%d ", node.Val)
+		}
+		fmt.Printf("] Queue: [ ")
+		for _, vertex := range queue {
+			fmt.Printf("%d ", vertex.node.Val)
+		}
+		fmt.Printf("]\n")
+	}
+
+	return root
+}
+
+func CallBFS() {
+	root := &Node{Val: 1}
+	root.Left = &Node{Val: 2}
+	root.Right = &Node{Val: 3}
+	root.Left.Left = &Node{Val: 4}
+	root.Left.Right = &Node{Val: 5}
+	root.Right.Left = &Node{Val: 6}
+	root.Right.Right = &Node{Val: 7}
+
+	fmt.Println("BFS traversal of the binary tree:")
+	BFS(root)
+
+	// The result
+	// BFS traversal of the binary tree:
+	// Visited: [ 1 ] Queue: [ 2 3 ]
+	// Visited: [ 1 2 ] Queue: [ 3 4 5 ]
+	// Visited: [ 1 2 3 ] Queue: [ 4 5 6 7 ]
+	// Visited: [ 1 2 3 4 ] Queue: [ 5 6 7 ]
+	// Visited: [ 1 2 3 4 5 ] Queue: [ 6 7 ]
+	// Visited: [ 1 2 3 4 5 6 ] Queue: [ 7 ]
+	// Visited: [ 1 2 3 4 5 6 7 ] Queue: [ ]
 }
